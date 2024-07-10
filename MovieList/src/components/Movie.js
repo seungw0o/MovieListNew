e868775bfb94dc45f04c2365288463f6c49d719e
정승우 <ssounsowns@gmail.com>
2024-07-10 09:26:35 +0900

    Movie 함수에 styled-component 적용 완
    2024/07/10

--- MovieList/src/components/Movie.js
+++ MovieList/src/components/Movie.js
@@ -16,27 +16,72 @@ const Movie = styled.div`
   color: inherit;
   box-shadow: 0 13px 27px -5px rgba(50, 50, 93, 0.25),
     0 8px 16px -8px rgba(0, 0, 0, 0.3), 0 -6px 16px -6px rgba(0, 0, 0, 0.025);
+  height: fit-content;
 `;
 
 const MovieImg = styled.img`
   position: relative;
   top: -50px;
   max-width: 150px;
+  height: 200px;
   width: 100%;
   margin-right: 30px;
   box-shadow: 0 30px 60px -12px rgba(50, 50, 93, 0.25),
     0 18px 36px -18px rgba(0, 0, 0, 0.3), 0 -12px 36px -8px rgba(0, 0, 0, 0.025);
 `;
+const MovieTitle = styled.h2`
+  margin: 0;
+  font-weight: 300;
+  text-decoration: none;
+  & > a {
+    margin-bottom: 5px;
+    font-size: 24px;
+    color: #2c2c2c;
+    text-decoration: none;
+  }
+`;
+const MovieYear = styled.h3`
+  margin: 0;
+  font-weight: 300;
+  text-decoration: none;
+  margin-right: 10px;
+  font-size: 14px;
+`;
+const MovieSummary = styled.p``;
+const MovieGenres = styled.ul`
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  display: flex;
+  flex-wrap: wrap;
+  margin: 5px 0px;
+  & > li {
+    margin-right: 10px;
+    font-size: 14px;
+  }
+`;
 
-function MovieScreen({ id, coverImg, title, year, summary, genres }) {
-  <Movie>
-    <h1>hellofsafs</h1>
-  </Movie>;
+function MovieScreen({ id, coverimg, title, year, summary, genres }) {
+  return (
+    <Movie>
+      <MovieImg src={coverimg} alt={title}></MovieImg>
+      <MovieTitle>{title}</MovieTitle>
+      <div>
+        <MovieYear>{year}</MovieYear>
+        <MovieSummary>{summary}</MovieSummary>
+        <MovieGenres>
+          {genres.map(g => (
+            <li key={g}>{g}</li>
+          ))}
+        </MovieGenres>
+      </div>
+    </Movie>
+  );
 }
 
 Movie.propTypes = {
   id: PropTypes.number.isRequired,
-  coverImg: PropTypes.string.isRequired,
+  coverimg: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
   genres: PropTypes.arrayOf(PropTypes.string).isRequired,
